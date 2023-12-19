958b212ba6692f5e0e10fbf888b1a47b351dd01f
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2023-12-19 15:01:57 +0900

    랜덤다이스

---
+++ client/lib/utils/index.js
@@ -0,0 +1,3 @@
+export * from './memo.js';
+export * from './copy.js';
+export * from './typeOf.js';
---
+++ client/lib/utils/memo.js
@@ -0,0 +1,13 @@
+export const memo = (() => {
+  const cache = {};
+
+  return (key, callback) => {
+    if (!callback) return cache[key];
+
+    if (cache[key]) {
+      console.warn(`${key} 안에는 이미 캐시된 값이 존재합니다.`);
+      return cache[key];
+    }
+    cache[key] = callback();
+  };
+})();
